51d8c584093be96f2d538bef1677d38ab8620d4d
ckaminsk <ckaminsk@nbbedckaminsk.bedford.progress.com>
2012-06-19 16:26:23 -0400

    Ajax query working.

--- src/main/java/com/darthcoder/JMSTestServlet/MainServlet.java
+++ src/main/java/com/darthcoder/JMSTestServlet/MainServlet.java
@@ -1,8 +1,57 @@
 package com.darthcoder.JMSTestServlet;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NameClassPair;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
 import javax.servlet.http.*; 
+
+import com.sun.xml.rpc.processor.modeler.j2ee.xml.javaXmlTypeMappingType;
 
 @SuppressWarnings("serial")
 public class MainServlet extends HttpServlet {
+	private String getJMSNames() { 
+		StringBuilder retVal = new StringBuilder();
+		try {
+			InitialContext ctx = new InitialContext();
+			
+			System.out.println("Retrieved initialContext..."); 
+			
+			NamingEnumeration ne = ctx.list("jms"); 
+			if ( null != ne ) { 
+				System.out.println("We have naming enumeration..."); 
+				while ( ne.hasMore() ) { 
+					NameClassPair entry = (NameClassPair)ne.next();
+					System.out.println("name: " + entry.getName() + " class: " +  entry.getClassName());
+					if ( retVal.length() > 0 ) retVal.append(";");
+					retVal.append(entry.getName()); 
+				}
+			}
+			
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
 
+		return retVal.toString();
+	} 
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) { 
+		String action = request.getParameter("action"); 
+		if ( action.equals("jmsdata") ) { 
+			String jmsdata = getJMSNames(); 
+			response.setContentType("text/text"); 
+			try {
+				response.getWriter().write(jmsdata);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
+		} else { 
+			
+		}
+	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,25 @@
 <html>
+<head>
+ <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script> 
+ <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js" ></script> 
+ 
+<script type="text/javascript"> 
+	$(document).ready(function() {
+	   $.get('./jmsops/?action=jmsdata', function(data) { 
+		  alert("data received: " + data); 
+	   });
+	 });
+</script>
+
+
+</head>
 <body>
-<h2>Hello World!</h2>
+Connection Factory: <select name="CFactory"> <option value=""/> </select>  Custom: <input type="text" name="CFactoryCustom"/> <br/>
+Source: <select name="Source"> <option value="" /> </select>  Custom: <input type="text" name="SourceCustom"/> <br/>
+ReplyTo:  <select name="ReplyTo"> <option value=""/> </select>  Custom: <input type="text" name="ReplyToCustom"/> <br/>
+<input type="radio" name="ConnectionType" value="Queue">Queue</input> 
+<input type="radio" name="ConnectionType" value="Topic">Topic</input> 
+<br/> 
+ 
 </body>
 </html>
